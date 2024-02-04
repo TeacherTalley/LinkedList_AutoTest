b3e17493ea91063dffad2b060ecfd1970d366d7b
TeacherTalley <ptalley2@uca.edu>
2024-02-04 10:17:17 -0600

    refining style exceptions

--- AutoTest_Style.sh
+++ AutoTest_Style.sh
@@ -7,6 +7,6 @@ echo "Source files:" $srcfiles
 
 # for some reason, GitHub Classroom environment does not use cpplint.cfg
 # explcitly ignore some style checks
-filters=-legal/copyright,-build/header_guard,-whitespace/braces,-runtime/explicit,-whitespace/newline,-whitespace/end_of_line,-whitespace/blank_line,-whitespace/indent
+filters=-legal/copyright,-build/header_guard,-whitespace/braces,-runtime/explicit,-whitespace/newline,-whitespace/end_of_line,-whitespace/blank_line,-whitespace/indent,-whitespace/comments
 
 cpplint --filter=$filters $srcfiles
