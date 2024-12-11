89900eb37e44e4cafc0744cf41a7290ddedaf91b
Terry Talley <ttalley@cub.uca.edu>
2024-12-11 12:54:17 -0600

    2025 Spring

--- AutoTest_all.sh
+++ AutoTest_all.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#--------------------------------------------------------------------------
+# File: AutoTest_all.sh
+# Programmer: Michelle Talley
+# Copyright 2024 Michelle Talley University of Central Arkansas
+#--------------------------------------------------------------------------
 repo=LinkedList_AutoTest
 echo "#################### START: AutoTest Setup ##################################"
 echo " To be consistent with the grading environment, assume we are starting out "
@@ -8,33 +13,43 @@ echo "##########################################################################
 cd $repo
 echo
 echo "#################### START: AutoTest Results #####################"
-echo "--- Checking code format (cpplint) ---"
-./AutoTest_Style.sh
+./AutoTest_Style.sh $repo main.cpp LinkedList.h
 echo
-echo "--- Checking main output (diff) ---"
 cd build
-../AutoTest_OutputTest.sh
+../AutoTest_OutputTest.sh $repo/build
 echo
 echo "--- Unit testing (googletest - all tests at once) ---"
 ctest
 echo
+# Note: GitHub Classroom auto-grading detects non-zero exit codes as errors and reports.
+# As a result, AutoTest_gtests can be run directly in the auto-grading environment.
+# The AutoTest_single_gtest script runs the individual tests a nicer error message for
+# use when testing locally (i.e., in a shell).
+#
+# GitHub Classroom auto-grading runs the following commands from the current
+# directory of the project being tested.  To similate that here, we need to
+# change to the project directory before running the tests.
+#
+cd ../..
 echo "--- Unit testing (single test at a time) ---"
-./AutoTest_gtests --gtest_filter=LinkedListTest.Empty
-./AutoTest_gtests --gtest_filter=LinkedListTest.Size
-./AutoTest_gtests --gtest_filter=LinkedListTest.PushBackandBack
-./AutoTest_gtests --gtest_filter=LinkedListTest.PopBack
-./AutoTest_gtests --gtest_filter=LinkedListTest.PopFront
-./AutoTest_gtests --gtest_filter=LinkedListTest.PushFrontandFront
-./AutoTest_gtests --gtest_filter=LinkedListTest.Insert
-./AutoTest_gtests --gtest_filter=LinkedListTest.Erase
-./AutoTest_gtests --gtest_filter=LinkedListTest.FrontException
-./AutoTest_gtests --gtest_filter=LinkedListTest.BackException
-./AutoTest_gtests --gtest_filter=LinkedListTest.InsertException
-./AutoTest_gtests --gtest_filter=LinkedListTest.PopBackException
-./AutoTest_gtests --gtest_filter=LinkedListTest.PopFrontException
-./AutoTest_gtests --gtest_filter=LinkedListTest.Print
+# Note: The following commands should be exactly the same as specified in classroom.yml
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.Empty
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.Empty
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.Size
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.PushBackAndBack
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.PopBack
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.PopFront
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.PushFrontAndFront
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.Insert
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.Erase
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.FrontException
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.BackException
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.InsertException
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.PopBackException
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.PopFrontException
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.EraseException
+./LinkedList_AutoTest/AutoTest_gtest.sh LinkedListTest.Print
 
 echo
-cd ..
 echo "#################### END: AutoTest Results   #####################"
 echo
--- AutoTest_gtests.cpp
+++ AutoTest_gtests.cpp
@@ -16,11 +16,14 @@
 #include <string>
 #include "LinkedList.h"
 
+#define GTEST_COUT std::cerr << "[          ] [ INFO ]"
+
 // Test fixture for LinkedList class
 class LinkedListTest : public ::testing::Test
 {
 protected:
     LinkedList<int> list;
+    LinkedList<std::string> stringList;
 
     void SetUp() override
     {
@@ -42,9 +45,20 @@ TEST_F(LinkedListTest, Empty)
 // Test case for size() method
 TEST_F(LinkedListTest, Size)
 {
+    std::string message = " ";
+
+    message = " Size on a list with elements";
+    GTEST_COUT << message << std::endl;
     EXPECT_EQ(list.size(), 3);
     list.pop_back();
     EXPECT_EQ(list.size(), 2);
+    list.pop_back();
+    EXPECT_EQ(list.size(), 1);
+
+    message = " Size on an empty list";
+    GTEST_COUT << message << std::endl;
+    list.pop_back();
+    EXPECT_EQ(list.size(), 0);
     list.clear();
     EXPECT_EQ(list.size(), 0);
 }
@@ -52,61 +66,177 @@ TEST_F(LinkedListTest, Size)
 // Test case for push_back() and back() methods
 TEST_F(LinkedListTest, PushBackAndBack)
 {
-    list.push_back(4);
-    EXPECT_EQ(list.back(), 4);
+    std::string message = " ";
+
+    list.clear();
+
+    message = " PushBack on an empty list";
+    GTEST_COUT << message << std::endl;
+    list.push_back(1);
+    EXPECT_EQ(list.size(), 1);
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 1);
+
+    message = " PushBack on list with 1 element";
+    GTEST_COUT << message << std::endl;
+    list.push_back(2);
+    EXPECT_EQ(list.size(), 2);
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 2);
+
+    message = " PushBack on list with 2 elements";
+    GTEST_COUT << message << std::endl;
+    list.push_back(3);
+    EXPECT_EQ(list.size(), 3);
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 3);
 }
 
 // Test case for pop_back() method
 TEST_F(LinkedListTest, PopBack)
 {
+    std::string message = " ";
+
+    message = " PopBack on a list with 3 elements";
+    GTEST_COUT << message << std::endl;
     list.pop_back();
+    EXPECT_EQ(list.size(), 2);
+    EXPECT_EQ(list.front(), 1);
     EXPECT_EQ(list.back(), 2);
-}
+
+    message = " PopBack on a list with 2 elements";
+    GTEST_COUT << message << std::endl;
+    list.pop_back();
+    EXPECT_EQ(list.size(), 1);
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 1);
+
+    message = " PopBack on a list with 1 element";
+    list.pop_back();
+    EXPECT_EQ(list.size(), 0);
+
+    message = " Front on empty list after PopBack";
+    GTEST_COUT << message << std::endl;
+    EXPECT_THROW(list.front(), std::out_of_range);
+ }
 
 // Test case for pop_front() method
 TEST_F(LinkedListTest, PopFront)
 {
+    std::string message = " ";
+
+    message = " PopFront on a list with 3 elements";
+    GTEST_COUT << message << std::endl;
     list.pop_front();
+    EXPECT_EQ(list.size(), 2);
     EXPECT_EQ(list.front(), 2);
+    EXPECT_EQ(list.back(), 3);
+
+    message = " PopFront on a list with 2 elements";
+    GTEST_COUT << message << std::endl;
+    list.pop_front();
+    EXPECT_EQ(list.size(), 1);
+    EXPECT_EQ(list.front(), 3);
+    EXPECT_EQ(list.back(), 3);
+
+    message = " PopFront on a list with 1 element";
+    GTEST_COUT << message << std::endl;
+    list.pop_front();
+    EXPECT_EQ(list.size(), 0);
+
+    message = " Front on empty list after PopFront";
+    GTEST_COUT << message << std::endl;
+    EXPECT_THROW(list.front(), std::out_of_range);
 }
 
 // Test case for push_front() and front() methods
 TEST_F(LinkedListTest, PushFrontAndFront)
 {
-    list.push_front(0);
-    EXPECT_EQ(list.front(), 0);
+    std::string message = " ";
+
+    list.clear();
+
+    message = " PushFront on an empty list";
+    GTEST_COUT << message << std::endl;
+    list.push_front(3);
+    EXPECT_EQ(list.size(), 1);
+    EXPECT_EQ(list.front(), 3);
+    EXPECT_EQ(list.back(), 3);
+
+    message = " PushFront on a list with 1 element";
+    GTEST_COUT << message << std::endl;
+    list.push_front(2);
+    EXPECT_EQ(list.size(), 2);
+    EXPECT_EQ(list.front(), 2);
+    EXPECT_EQ(list.back(), 3);
+
+    message = " PushFront on a list with 2 elements";
+    GTEST_COUT << message << std::endl;
+    list.push_front(1);
+    EXPECT_EQ(list.size(), 3);
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 3);
 }
 
 // Test case for insert() method
 TEST_F(LinkedListTest, Insert)
 {
-    // insert at second position
+    std::string message = " ";
+    
+    message = " Insert at second position";
+    GTEST_COUT << message << std::endl;
     list.insert(1, 5);
+    // Note: The following line will not print the message if the test passes,
+    // however, this mechanism works to add messages to errors.
+    // Leaving it in for demonstration purposes.
+    // EXPECT_EQ(list.size(), 4) << message;
     EXPECT_EQ(list.size(), 4);
     EXPECT_EQ(list.front(), 1);
     EXPECT_EQ(list.back(), 3);
     list.pop_front();
+    EXPECT_EQ(list.size(), 3);
     EXPECT_EQ(list.front(), 5);
     EXPECT_EQ(list.back(), 3);
-    // insert at end
-    list.insert(list.size(), 6);
+    
+    message = " Insert at end";
+    GTEST_COUT << message << std::endl;
+    list.insert(list.size(), 4);
+    EXPECT_EQ(list.size(), 4);
     EXPECT_EQ(list.front(), 5);
-    EXPECT_EQ(list.back(), 6);
-    // insert at first position
+    EXPECT_EQ(list.back(), 4);
+ 
+    message = " Insert at front";
+    GTEST_COUT << message << std::endl;
     list.insert(0, 7);
+    EXPECT_EQ(list.size(), 5);
     EXPECT_EQ(list.front(), 7);
-    EXPECT_EQ(list.back(), 6);
+    EXPECT_EQ(list.back(), 4);
     list.pop_front();
+    EXPECT_EQ(list.size(), 4);
     EXPECT_EQ(list.front(), 5);
+    EXPECT_EQ(list.back(), 4);
 }
 
 // Test case for erase() method
 TEST_F(LinkedListTest, Erase)
 {
+    std::string message = " ";
+
+    message = " Erase from a list with elements";
+    GTEST_COUT << message << std::endl;
     list.erase(1);
     EXPECT_EQ(list.size(), 2);
     EXPECT_EQ(list.front(), 1);
     EXPECT_EQ(list.back(), 3);
+    list.erase(1);
+    EXPECT_EQ(list.size(), 1);
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 1);
+
+    message = " Erase from an empty list";
+    GTEST_COUT << message << std::endl;
+    list.erase(0);
+    EXPECT_EQ(list.size(), 0);
 }
 
 // Test case for front() method exception
@@ -126,8 +256,21 @@ TEST_F(LinkedListTest, BackException)
 // Test case for insert() method exception
 TEST_F(LinkedListTest, InsertException)
 {
+    std::string message = " ";
+
+    message = " Insert beyond the end of the list";
+    GTEST_COUT << message << std::endl;
     EXPECT_THROW(list.insert(4, 5), std::out_of_range);
+    EXPECT_EQ(list.size(), 3);
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 3);
+
+    message = " Insert before the start of the list";
+    GTEST_COUT << message << std::endl;
     EXPECT_THROW(list.insert(-1, -1), std::out_of_range);
+    EXPECT_EQ(list.size(), 3);
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 3);
 }
 
 // Test case for pop_back() method exception
@@ -137,11 +280,25 @@ TEST_F(LinkedListTest, PopBackException)
     EXPECT_THROW(list.pop_back(), std::out_of_range);
 }
 
-// Test case for pop_back() method exception
+// Test case for pop_front() method exception
 TEST_F(LinkedListTest, PopFrontException)
 {
     list.clear();
     EXPECT_THROW(list.pop_front(), std::out_of_range);
+}
+
+// Test case for erase() method exception
+TEST_F(LinkedListTest, EraseException)
+{
+    EXPECT_THROW(list.erase(4), std::out_of_range);
+    EXPECT_EQ(list.size(), 3);
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 3);
+
+    EXPECT_THROW(list.erase(-1), std::out_of_range);
+    EXPECT_EQ(list.size(), 3);
+    EXPECT_EQ(list.front(), 1);
+    EXPECT_EQ(list.back(), 3);
 }
 
 // Test case for print() method
@@ -152,4 +309,3 @@ TEST_F(LinkedListTest, Print)
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "1 2 3\n");
 }
-
